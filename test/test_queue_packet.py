6f4d1b64fae4ce26960b45e0b6fe1a5a2558c0c6
Artem Karpov <artem_karpov@lesta.group>
2025-06-04 14:02:19 +0300

    packet queue

---
+++ test/test_queue_packet.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("D:/_Others/rpc-python-server")
+
+# import packet_queue as pq
+
+
+def test_pq() -> None:
+    # items = [1, 2, 3]
+    # for i in items:
+    #     pq.queue.append(i)
+
+    assert True
