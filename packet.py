360ac39babf644720bb2b5fcdecb1652ea302d35
Artem Karpov <artem_karpov@lesta.group>
2025-06-04 11:44:29 +0300

    Packet

---
+++ packet.py
@@ -0,0 +1,22 @@
+import json
+
+
+class Packet(object):
+    def __init__(self, msg: str) -> None:
+        self.__msg: str = msg
+        self.recepient: int
+        self.action: str
+        self.payload: list
+
+    def extend(self) -> None:
+        src = json.loads(self.__msg)
+        self.recepient = src[0]
+        self.action = src[1]
+        self.payload = src[2:]
+
+    def __str__(self) -> str:
+        raw_msg = [self.recepient, self.action, *self.payload]
+        return json.dumps(raw_msg)
+
+    def __byte__(self) -> bytes:
+        return str(self).encode("utf-8")
