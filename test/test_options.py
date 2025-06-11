c0569e12b02865acd85a668a29f6ecca03ad5605
saeegonee <saeegonee@yandex.ru>
2025-06-11 15:49:12 +0300

    extend options test

--- test/test_options.py
+++ test/test_options.py
@@ -12,8 +12,18 @@ def test_options_type() -> None:
     assert isinstance(opt.TOKEN, str)
     assert isinstance(opt.ROOM_TIMEOUT, int)
 
-def test_options() -> None:
+def test_options_address() -> None:
+    l_addr = opt.ADDRESS.split(".")
     assert len(opt.ADDRESS) > 0
+    assert len(l_addr) == 4
+    for i in l_addr:
+        assert isinstance(int(i), int)
+
+def test_options_port() -> None:
+    forbiden = [7, 20, 21, 22, 23, 25, 53, 69, 80, 88, 102, 110, 135, 137, 139, 143, 443, 993]
     assert opt.PORT > 0
+    assert not opt.PORT in forbiden
+
+def test_options() -> None:
     assert len(opt.TOKEN) > 0
     assert opt.ROOM_TIMEOUT > 20
