4974b6b653bb88a05a9d4ead6d8c92b7f5535ac2
saeegonee <saeegonee@yandex.ru>
2025-06-11 14:47:27 +0300

    client send custom msg

--- client/client.py
+++ client/client.py
@@ -6,8 +6,11 @@ async def listen() -> None:
     url = "ws://127.0.0.1:1909"
     async with websockets.connect(url) as ws:
         while True:
-            await ws.send(f'[1,"auth","token"]')
-            await asyncio.sleep(0.4)
+            inp = input("<target>,<func>,<payload>: ")
+            l_pack = inp.split(",")
+
+            await ws.send(f'[{l_pack[0]},"{l_pack[1]}","{l_pack[2]}"]')
+            await ws.recv()
 
 
 asyncio.get_event_loop().run_until_complete(listen())
