fe0a81031b6bae2ce59a2752ffa489b6ad50f6e2
saeegonee <saeegonee@yandex.ru>
2025-06-11 14:47:27 +0300

    modern auth

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ import websockets
 from options import Option
 from messages import Message
 from packet import Packet
-from peer import Peer
 from peer_server import PeerServer
 
 
@@ -18,35 +17,40 @@ logging.basicConfig(
 )
 
 
-clients = set()
-serv_peer = PeerServer(clients)
-peers = { 
-    0: set(),
-    1: serv_peer
-}
+peers = {0: set()}
+serv_peer = PeerServer(peers)
+peers[1] = serv_peer
 
 
 async def echo(socket) -> None:
     """Server func."""
 
-    idx = len(peers[0]) + 2
-    peer = Peer(socket, idx)
-    peers[0].add(peer)
-    peers[idx] = peer
-    log.info(msg.connect(idx))
+    # idx = len(peers[0]) + 2
+    # peer = Peer(socket, idx)
+    # peers[0].add(peer)
+    # peers[idx] = peer
+    # log.info(msg.connect(idx))
 
     try:
         async for ws in socket:
             packet = Packet(ws)
             packet.extend()
-            
+
+            if hasattr(peers[packet.recepient], packet.action):
+                # TODO. call action by name
+                pass
+
+            else:
+                # TODO. send pity request
+                pass
+
             log.info(msg.receive_msg(str(packet)))
 
     except websockets.ConnectionClosed as err:
-        peers[0].remove(peer)
-        del peers[idx]
+        # peers[0].remove(peer)
+        # del peers[idx]
 
-        log.warning(msg.disconnect(idx, err))
+        log.warning(msg.disconnect(-1, err))
 
 
 async def server_task(addr: str, port: int) -> None:
@@ -70,9 +74,9 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    
+
     args = sys.argv[1:]
-    if args.__contains__("-v"):
+    if "-v" in args:
         logging.basicConfig(level=logging.INFO)
 
     asyncio.run(main())
