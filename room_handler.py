c7f2fd8dc814c7f476b61b62a95f2e8f0874a26f
saeegonee <saeegonee@yandex.ru>
2025-06-11 17:17:10 +0300

    upd

--- room_handler.py
+++ room_handler.py
@@ -1,6 +1,20 @@
+from stuff.options import TOKEN
+
+
 class RoomHandlerCommon(object):
-    def auth(self, token: str) -> bool:
+
+    def _check_token(self, token: str) -> bool:
+        if token != TOKEN:
+            return False
         return True
 
-    def msg(self, txt: str) -> bool:
-        return True
+    def auth(self, token: str) -> None:
+        if self._check_token(token):
+            # TODO. Extend room
+            pass
+        else:
+            # TODO. Pity request
+            pass
+
+    def ready(self) -> None:
+        """Set ready for game."""
